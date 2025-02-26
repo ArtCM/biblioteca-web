e4a47d6353c12274449053e4e16e912726ccdf1f
ArtCM <arthurperfilprof@hotmail.com>
2025-02-26 19:45:40 -0300

    feat: adicionado o componente footer e adicionado a expansão da tag main para preencher a altura da tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
 
 export default function App() {
@@ -5,7 +6,12 @@ export default function App() {
   return (
     <>
       <Header />
-      <h1>Gerenciador de Livros</h1>
+
+      <main>
+        <h1>Gerenciador de Livros</h1>
+      </main>
+      
+      <Footer />
     </>
   );
 }
