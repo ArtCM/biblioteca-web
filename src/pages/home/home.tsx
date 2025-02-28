2468d62826217d36cae0c92d0962fb7da600a90e
ArtCM <arthurperfilprof@hotmail.com>
2025-02-28 14:50:33 -0300

    fix: ajuste na responsividade do video, ele não será exibido no celular pelo carregamento demorado

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,27 +3,27 @@ import "./home.css";
 
 export default function Home() {
   const [showTitle, setShowTitle] = useState(false);
+  const [showVideo, setShowVideo] = useState(window.innerWidth > 900);
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    const video = videoRef.current;
-    if (!video) return;
+    const titleTimeout = setTimeout(() => setShowTitle(true), 300);
 
-    const handleVideoEnd = () => {
-      setShowTitle(true);
-    };
-
-    video.play().catch((error) => console.error("Erro ao reproduzir o vídeo:", error));
-    video.addEventListener("ended", handleVideoEnd);
-
-    return () => {
-      video.removeEventListener("ended", handleVideoEnd);
-    };
+    return () => clearTimeout(titleTimeout);
   }, []);
+
+  useEffect(() => {
+    if (!showVideo || !videoRef.current) return;
+
+    const video = videoRef.current;
+    video.play().catch((error) => console.error("Erro ao reproduzir o vídeo:", error));
+  }, [showVideo]);
 
   return (
     <main className="home-container">
-      <video ref={videoRef} className="background-video" src="/assets/video/bg-video.mp4" autoPlay muted />
+      {showVideo && (
+        <video ref={videoRef} className="background-video" src="/assets/video/bg-video.mp4" autoPlay muted />
+      )}
 
       {showTitle && 
         <div className="home-container__content">
